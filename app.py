ded8dd5efcb2397df9facde6e3f0949532b53781
Mostafa Abdullhuq <mostafaabdullhuq@gmail.com>
2022-07-25 01:48:36 +0200

    Initializing Project Files And Content

---
+++ app.py
@@ -0,0 +1,39 @@
+import os
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+
+
+app = Flask(__name__)
+
+
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///project.db")
+
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
